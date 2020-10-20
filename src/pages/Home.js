29f51b44ab4a909aae1c4cde5532c5e98ddaf712
ChoSeungHei <kwa0403261@gmail.com>
2020-10-20 14:53:44 +0900

    feat: add sidebar list

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,30 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import { FiMenu,FiChevronRight,FiX } from "react-icons/fi";
 import { Link } from 'react-router-dom';
+import Listview from '../template/Listview';
 
 const Home = () => {
     const [isopen,setIsopen] = useState(false);
-
+    const [email,setEmail] = useState('');
+    const [name,setName] = useState('');
+    const [isLogin,setIsLogin] = useState(false);
+    
     const goMenu = () => {
         setIsopen(!isopen);
     }
 
+    useEffect(() => {
+        var test = localStorage.getItem("email");
+        var test2 = localStorage.getItem("name");
+        if(test !== null && test2 !== null)
+        {
+            setEmail(test);
+            setName(test2);
+            setIsLogin(true);
+        }
+    },[]);
+  
+    const items = [{id:1,title:"로그아웃"}];
     return (
         <div>
             {
@@ -20,8 +36,12 @@ const Home = () => {
                                     <FiX size="1.5em" onClick={goMenu}/>
                                 </div>  
                             </div>
-                            <div className="login_box">
-                                
+                            <div className="login_box vertical-center">
+                                <div className="m-3">
+                                    {name}<br/> 
+                                    등록리뷰: 0건
+                                </div>
+                                <Listview items={items}/>
                             </div>
                         </div>
                         <div className="div1" onClick={goMenu}></div>
@@ -37,10 +57,18 @@ const Home = () => {
                 </div>
             </div>
             <div className="login_div vertical-center">
-                <div className="m-4">
-                    <FiChevronRight size={"1.5em"}/>
-                    <Link to="/login"><span className="align-middle link_style">로그인 해주세요</span></Link>
-                </div>
+                {
+                    isLogin? (
+                    <div className="m-4">
+                        {name}님 안녕하세요.
+                    </div>
+                    ):(
+                        <div className="m-4">
+                            <FiChevronRight size={"1.5em"}/>
+                            <Link to="/login"><span className="align-middle link_style">로그인 해주세요</span></Link>
+                        </div>
+                    )
+                }
             </div>
         </div>
     );
---
+++ src/template/Listview.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Listview = ({items}) => {
+    
+    return(
+        <div>
+            {items[0].title}
+        </div>
+    );
+}
+
+export default Listview;
